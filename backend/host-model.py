52b28fccbc9f434d948696933c2a3db799647dfe
josephricgarcia <josephricgarcia@gmail.com>
2025-11-05 03:04:38 +0800

    11-5

--- backend/host-model.py
+++ backend/host-model.py
@@ -2,6 +2,7 @@ from flask import Flask, request, jsonify
 import joblib
 import numpy as np
 from flask_cors import CORS
+import os
 
 app = Flask(__name__)
 CORS(app)
@@ -40,4 +41,5 @@ def predict():
 
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
